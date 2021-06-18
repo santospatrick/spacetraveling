be4efcf53c827760b0219c197252252fae0bbe2d
Patrick Santos <patrickspteixeira@gmail.com>
2021-06-17 22:56:55 -0300

    add home styling and header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import headerStyles from './header.module.scss';
+import styles from '../../styles/common.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={headerStyles.container}>
+      <div className={styles.container}>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
-// import styles from './home.module.scss';
+import styles from './home.module.scss';
 
 interface Post {
   uid?: string;
@@ -26,13 +27,32 @@ interface HomeProps {
 }
 
 export default function Home(): JSX.Element {
+  const arr = [1, 2, 3];
   return (
-    <>
-      <Head>
-        <title>Início | spacetraveling</title>
-      </Head>
-      <div>home</div>
-    </>
+    <div className={commonStyles.container}>
+      {arr.map(() => (
+        <div className={styles.post}>
+          <div className={styles.postTitle}>Como utilizar Hooks</div>
+          <div className={styles.postSubtitle}>
+            Pensando em sincronização em vez de ciclos de vida.
+          </div>
+          <div className={styles.postFooter}>
+            <div className={styles.postFooterItem}>
+              <FiCalendar />
+              <span>15 Mar 2021</span>
+            </div>
+            <div className={styles.postFooterItem}>
+              <FiUser />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <button type="button" className={styles.loadMore}>
+        Carregar mais posts
+      </button>
+    </div>
   );
 }
 
